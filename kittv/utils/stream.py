6fd9559281e3c1d2c527c8b95e6d4718d39c2bff
Mingzhe Zou <zoumingzhe@outlook.com>
2025-01-24 02:30:14 +0800

    utils.stream: enhance StreamProber

--- kittv/utils/stream.py
+++ kittv/utils/stream.py
@@ -9,11 +9,14 @@ from ffmpeg import probe as ffprobe
 from ipytv.playlist import IPTVAttr
 from ipytv.playlist import IPTVChannel
 import requests
-from xkits import CacheAtom
+from xkits import CacheTimeout
+from xkits import NamedCache
 
 
-class StreamProber(CacheAtom[Dict[str, Any]]):
-    PERIOD = 300
+class StreamProber(NamedCache[str, Dict[str, Any]]):
+    MINIMUM = 1800  # 30 minutes
+    DEFAULT = 10800  # 3 hours
+    MAXIMUM = 86400  # 1 day
 
     class Format:
         def __init__(self, data: Dict[str, Any]):
@@ -23,10 +26,12 @@ class StreamProber(CacheAtom[Dict[str, Any]]):
         def probe_score(self) -> int:
             return self.__data["probe_score"]
 
-    def __init__(self, data: Optional[Dict[str, Any]] = None):
-        super().__init__(data=data or {}, lifetime=self.PERIOD)
+    def __init__(self, timeout: float, lifetime: CacheTimeout,
+                 url: str, data: Optional[Dict[str, Any]] = None):
+        super().__init__(name=url, data=data or {}, lifetime=lifetime)
         self.__format: StreamProber.Format = self.Format(self.data.get("format", {}))  # noqa:E501
         self.__success: bool = data is not None
+        self.__timeout: float = timeout
 
     @property
     def success(self) -> bool:
@@ -36,12 +41,28 @@ class StreamProber(CacheAtom[Dict[str, Any]]):
     def format(self) -> Format:
         return self.__format
 
+    @property
+    def object(self) -> "StreamProber":
+        if not self.expired:
+            return self
+
+        def new_timeout(timeout: float) -> float:
+            return min(timeout if self.success else timeout + 1.0, 60.0)
+
+        def new_lifetime(timeout: float, lifetime: float) -> float:
+            lifetime += 900 if self.success or timeout >= 30 else -lifetime / 4
+            return min(max(self.MINIMUM, lifetime), self.MAXIMUM)
+
+        timeout: float = new_timeout(self.__timeout)
+        return self.ffprobe(self.name, timeout, new_lifetime(timeout, self.life))  # noqa:E501
+
     @classmethod
-    def ffprobe(cls, url: str, timeout: float) -> "StreamProber":
+    def ffprobe(cls, url: str, timeout: float, lifetime: CacheTimeout = DEFAULT) -> "StreamProber":  # noqa:E501
         try:
-            return cls(ffprobe(url, timeout=int(timeout * 1000000)))
+            data = ffprobe(url, timeout=int(timeout * 1000000))
+            return cls(timeout, lifetime, url, data)
         except fferror:
-            return cls()
+            return cls(timeout, lifetime, url)
 
 
 class IPTVStream():
@@ -81,8 +102,9 @@ class IPTVStream():
 
     @property
     def probe(self) -> StreamProber:
-        if self.__probe is None or self.__probe.expired:
+        if self.__probe is None:
             self.__probe = StreamProber.ffprobe(self.url, self.timeout)
+        self.__probe = self.__probe.object
         return self.__probe
 
     @property
